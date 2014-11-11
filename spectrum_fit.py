13253aa92f660e3b35f23b941428cb45657b3f00
Owen <turner@roe.ac.uk>
2014-11-11 16:51:04 +0000

    all methods written and working

--- spectrum_fit.py
+++ spectrum_fit.py
@@ -145,12 +145,19 @@ def fitLines(flux, wavelength, z, weights):
 		new_wavelength = wavelength[index]
 		new_counts = counts[index]
 		new_weights = weights[index]
+		new_continuum = continuum[index]
 		new_index = np.where( new_wavelength < (line_dict[key] + 10))
 
 	#Select only data for the fit with these indices
 		fit_wavelength = new_wavelength[new_index]
 		fit_counts = new_counts[new_index]
 		fit_weights = new_weights[new_index]
+		fit_continuum = new_continuum[new_index]
+
+	#Compute the equivalent width
+		edge_continuum = fit_counts[0]
+		delta_lambda = abs(fit_wavelength[1] - fit_wavelength[0])	
+		E_w = sum(abs(1 - (fit_counts/edge_continuum))) * delta_lambda
 
 	#Now use the lmfit package to perform gaussian fits to the data	
 	#Construct the gaussian model
@@ -170,14 +177,14 @@ def fitLines(flux, wavelength, z, weights):
 	#Now perform the fit to the data using the set and guessed parameters 
 	#And the inverse variance weights form the fits file 
 		out  = mod.fit(fit_counts, pars, weights = fit_weights, x=fit_wavelength)
-		print(out.fit_report(min_correl=0.25))
+		#print(out.fit_report(min_correl=0.25))
 
 	#Plot the results and the spectrum to check the fit
 		plt.plot(fit_wavelength, out.best_fit, 'r-')
 	
 
 	#The amplitude parameter is the area under the curve, equivalent to the flux
-		results_dict[key] = [out.best_values['amplitude'], out.best_values['sigma'], 2.3548200*out.best_values['sigma']]
+		results_dict[key] = [out.best_values['amplitude'], out.best_values['sigma'], 2.3548200*out.best_values['sigma'], E_w]
 	
 
 	#The return dictionary for this method is a sequence of results vectors
@@ -191,58 +198,79 @@ def fitLines(flux, wavelength, z, weights):
 #
 #INPUTS:
 #
-#			flux: The full array of photon counts read in from a fits file
-#			wavelength: The corresponding wavelength values of the counts 
-#			z: The redshift of the source 		
-#			weights: Individual inverse variance weights on the fluxes
+#			flux_Ha: In units of ergs / s / cm^2
+#			flux_NeII6585: In units of ergs / s / cm^2
+#			z: redshift of the source
 #
-#OUTPUTS: 	dictionary: with keys corresponding to the names of the emission lines 
-#						each key contains 3 elements for the amplitude, sigma and fwhm
-#						of the fitted gaussians
 #
-#USAGE: 	results_dict = fitLines(flux, wavelength, z, weights)
-#######################################################################################
-#def galPhys():
+#OUTPUTS: 	dictionary: with keys corresponding to the SFR and metallicity of the galaxy 
+#						Note that the metallicity is in units 12 + log(O/H) and the 
+#						SFR is in units of log(Msun/yr)
+#
+#
+#USAGE: 	results_dict = galPhys(flux_Ha, flux_NeII6585)
+###########################################################################################
+
+def galPhys(flux_Ha, flux_NeII6585):
+
+	#Find the luminosity distance of the object at redshift z 
+	#Convert the luminosity distance to cm  
+	D_L = toolkit.cosmoDistanceQuad(z, 69.6, 0.286, 0.714)['D_L']
+	D_L = D_L * 3.086E26
+
+	#Compute H-alpha Luminosity in ergs / s
+	Lum_Ha = 4 * np.pi * (D_L**2) * flux_Ha
+
+	#Compute the star formation rate from the Kennicutt formula 
+	SFR = np.log10(Lum_Ha) - 41.27
+
+	#Compute the metallicity from the DO2 calibration
+	Met = 9.12 + (0.73 * (flux_NeII6585/flux_Ha))
+
+	return {'SFR' : SFR, 'Metallicity' : Met}
 
 
+
+
+
+
+#Read the names of the fits files in 
 data=np.genfromtxt('names.txt', dtype=None)
+
+#Loop over each name and apply the defined methods to each of them
+#Ultimately this is computing the physical properties of the galaxies
 for name in data:
+
+	#Read the fits files and define variables from the resulting dictionary
 	dict_results = readFits(name)
 	flux = dict_results['flux']		
 	wavelength = dict_results['wavelength']
 	z = dict_results['z']
 	weights = dict_results['weights']
 
-	#Fit the emission lines using the defined methods
+	#Fit the emission lines using the defined method, plot the results
 	fit_results = fitLines(flux, wavelength, z, weights)
-	print fit_results
-	plt.show()
+	#print fit_results
 
-	#The given flux is in 10^-17 ergs / s / cm^2
+	#Now show the plots if we want
+	#plt.show()
+
+	#The given flux is in 10^-17 ergs / s / cm^2, grab from the fitLines results
 	flux_Ha = fit_results['H_alpha'][0] * 1E-17 
+	flux_NeII6585 = fit_results['NII6585'][0] * 1E-17
 
-	#fitHbeta(flux, wavelength, z, continuum, error)
-	#fitOIIIfirst(flux, wavelength, z, continuum, error)
-	#fitOIIIsecond(flux, wavelength, z, continuum, error)
+	#Now use the galPhys method to compute the physical properties 
+	props = galPhys(flux_Ha, flux_NeII6585)
+	print props
 
-	#Find the luminosity distance of the object at redshift z 
-	#Convert the luminosity distance to cm  
-	D_L = toolkit.cosmoDistanceQuad(z, 69.6, 0.286, 0.714)['D_L']
 
-	D_L = D_L * 3.086E26
+
 	
 
-	#Compute H-alpha Luminosity in ergs / s
-	Lum_Ha = 4 * np.pi * (D_L**2) * flux_Ha
-	print Lum_Ha
 
-	#Compute the star formation rate from the Kennicutt formula 
-	SFR = np.log10(Lum_Ha) - 41.27
-	print SFR
-	print flux_Ha
+	
 
-
-	#since the above is a bit rubbish let's try fitting with lmfit 
+	 
 	
 
 
